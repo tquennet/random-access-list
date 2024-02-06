4c4dd8a33d3ea2a21b73bdfbb0c0448552eef848
Titouan Quennet <toureiffelProg@gmail.com>
2024-02-06 14:06:25 +0100

    append_l_r_eq_rev_append_rev_l_r

--- theories/structure/unary/List.v
+++ theories/structure/unary/List.v
@@ -40,11 +40,77 @@ Section List.
 	| x :: tleft => x :: (append tleft right)
 	end.
 
-	Fixpoint rev_append left right : List :=
-	match left with
-	| [] => right
-	| x :: t => rev_append t (x :: right)
+	Notation "l @ r" := (append l r) : unary_list_scope.
+
+	Lemma append_trans : forall l1 l2 l3 : List,
+		(l1 @ l2) @ l3 = l1 @ (l2 @ l3).
+	Proof.
+		intros l1 l2 l3.
+		{	induction l1 as [| a t1 H].
+		+	reflexivity.
+		+	simpl.
+			rewrite H.
+			reflexivity.
+		}
+	Qed.
+
+	Fixpoint rev_append l r : List :=
+	match l with
+	| [] => r
+	| x :: t => rev_append t (x :: r)
 	end.
+
+	Definition rev l := rev_append l [].
+
+	Lemma rev_append_append_r : forall l r : List,
+		rev_append l r = (rev_append l []) @ r.
+	Proof.
+		intro l.
+		{	induction l as [| x t H].
+		+	reflexivity.
+		+	intro r.
+			simpl.
+			rewrite (H (x :: r)), (H [x]).
+			rewrite append_trans.
+			reflexivity.
+		}
+	Qed.
+
+	Lemma rev_append_append_l : forall l1 l2 l3 : List,
+		rev_append (l1 @ l2) l3 = rev_append l2 (rev_append l1 l3).
+	Proof.
+		intros l1.
+		{	induction l1 as [| x t H].
+		+	reflexivity.
+		+	simpl.
+			intros l2 l3.
+			rewrite H.
+			reflexivity.
+		}
+	Qed.
+
+	Lemma rev_inv : forall l : List, rev (rev l) = l.
+	Proof.
+		intro l.
+		{	induction l as [| x t H].
+		+	reflexivity.
+		+	compute; fold rev_append; unfold rev in H.
+			rewrite (rev_append_append_r t).
+			rewrite (rev_append_append_l).
+			rewrite H.
+			reflexivity.
+		}
+	Qed.
+
+	Lemma append_l_r_eq_rev_append_rev_l_r : forall l r : List,
+		l @ r = rev_append (rev l) r.
+	Proof.
+		intros l r.
+		rewrite rev_append_append_r.
+		replace (rev_append (rev l) []) with (rev (rev l)) by reflexivity.
+		rewrite rev_inv.
+		reflexivity.
+	Qed.
 
 End List.
 
