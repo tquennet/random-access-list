55584d5a50bcb2911c2aea1c85a879915646a685
Titouan Quennet <titouanquennet@etu.u-paris.fr>
2024-04-17 15:21:37 +0200

    compare

--- theories/numerical/binary/BinNat.v
+++ theories/numerical/binary/BinNat.v
@@ -1,9 +1,10 @@
 Require Import Lists.List FunInd .
-(*Require Import Init.Nat.*)
+Require Import Init.Nat.
 Require Import utils.Utils.
 Import ListNotations.
 
 Declare Scope bin_nat_scope.
+Open Scope nat_scope.
 Open Scope bin_nat_scope.
 
 (********************************************************************************)
@@ -42,11 +43,27 @@ Fixpoint to_nat n : nat :=
 	| 1 :: t => S (2 * to_nat t)
 	end.
 
-Definition nat_equiv n m := to_nat n = to_nat m.
+(*Definition nat_equiv n m := to_nat n = to_nat m.
 
-Notation "n === m" := (nat_equiv n m) (at level 70, no associativity).
+Notation "n === m" := (nat_equiv n m) (at level 70, no associativity).*)
 
-Lemma to_nat_trail_0 : forall n, n ++ [0] === n.
+Lemma to_nat_app : forall n m, to_nat (n ++ m) = (to_nat n + to_nat m * 2 ^ length n).
+Proof.
+	intros n m.
+	{	induction n as [|bn tn HR]; [|destruct bn]; simpl.
+	+	rewrite PeanoNat.Nat.mul_1_r.
+		reflexivity.
+	+	rewrite HR, <- !plus_n_O, PeanoNat.Nat.mul_add_distr_l.
+		rewrite PeanoNat.Nat.add_shuffle0, !PeanoNat.Nat.add_assoc.
+		reflexivity.
+	+	f_equal.
+		rewrite HR, <- !plus_n_O, PeanoNat.Nat.mul_add_distr_l.
+		rewrite PeanoNat.Nat.add_shuffle0, !PeanoNat.Nat.add_assoc.
+		reflexivity.
+	}
+Qed.
+
+(*Lemma to_nat_trail_0 : forall n, n ++ [0] === n.
 Proof.
 	intro n.
 	unfold nat_equiv.
@@ -57,7 +74,7 @@ Proof.
 	+	rewrite HR.
 		reflexivity.
 	}
-Qed.
+Qed.*)
 
 Definition zero : t := [].
 
@@ -367,7 +384,534 @@ Proof.
 	rewrite inc_dec; assumption.
 Qed.
 
-Fixpoint sub_aux n m acc :=
+Notation rev_nat n := (to_nat (rev n)).
+
+Variant comp :=
+	| Eq : comp
+	| Lt : t -> dt -> dt -> comp
+	| Gt : t -> dt -> dt -> comp.
+
+Record decomp x y tn dn an :=
+{
+	dec_length : length an = length dn;
+	dec_zip : x = rev_append dn (1 :: tn);
+	dec_val : S (rev_nat an + to_nat y) = rev_nat (1 :: dn);
+}.
+
+Definition comp_op comp :=
+	match comp with
+	| Eq => Eq
+	| Lt rtn rdn ran => Gt rtn rdn ran
+	| Gt rtn rdn ran => Lt rtn rdn ran
+	end.
+
+Lemma comp_op_inv : forall comp, comp_op (comp_op comp) = comp.
+Proof.
+	intro comp.
+	destruct comp; reflexivity.
+Qed.
+Lemma comp_op_inj : forall c1 c2, comp_op c1 = comp_op c2 -> c1 = c2.
+Proof.
+	intros c1 c2 H.
+	destruct c1, c2; discriminate || (inversion H; reflexivity).
+Qed.
+
+
+Definition uc_Gt := uncurry (uncurry Gt).
+Definition uc_Lt := uncurry (uncurry Lt).
+
+Fixpoint compare_empty (n : t) (dn an : dt) :=
+	match n with
+	| [] => ([], [], [])
+	| 1 :: tn => (tn, dn, an)
+	| 0 :: tn => compare_empty tn (0 :: dn) (1 :: an)
+	end.
+
+
+Fixpoint compare_borrow_n (n m : t) (dn dm an am : dt) :=
+	match n, m with
+	| [], _ => None
+	| _, [] => Some (uc_Gt (compare_empty n dn an))
+	| [1], 1 :: tm => Some (Lt tm dm am)
+	| 1 as bn :: tn, 1 as bm :: tm | 0 as bn :: tn, 0 as bm :: tm
+		=> compare_borrow_n tn tm (bn :: dn) (bm :: dm) (1 :: an) (0 :: am) 
+	| 0 :: tn, 1 :: tm => compare_borrow_n tn tm (0 :: dn) (1 :: dm) (0 :: an) (1 :: am)
+	| 1 :: tn, 0 :: tm => compare_borrow_m tn tm (1 :: dn) (0 :: dm) (0 :: an) (1 :: am)
+	end
+with compare_borrow_m (n m : t) (dn dm an am : dt) :=
+	match n, m with
+	| _, [] => None
+	| [], _ => Some (uc_Lt (compare_empty m dm am))
+	| 1 :: tn, [1] => Some (Gt tn dn an)
+	| 1 as bn :: tn, 1 as bm :: tm | 0 as bn :: tn, 0 as bm :: tm
+		=> compare_borrow_m tn tm (bn :: dn) (bm :: dm) (0 :: an) (1 :: am)
+	| 0 :: tn, 1 :: tm => compare_borrow_n tn tm (0 :: dn) (1 :: dm) (1 :: an) (0 :: am)
+	| 1 :: tn, 0 :: tm => compare_borrow_m tn tm (1 :: dn) (0 :: dm) (1 :: an) (0 :: am)
+	end.
+
+Fixpoint compare (n m : t) (dn dm an am : dt) :=
+	match n, m with
+	| [], [] => Some (Eq)
+	| [], _ => Some (uc_Lt (compare_empty m dm am))
+	| _, [] => Some (uc_Gt (compare_empty n dn an))
+	| 0 as bn :: tn, 0 as bm :: tm | 1 as bn :: tn, 1 as bm :: tm
+		=> compare tn tm (bn :: dn) (bm :: dm) (1 :: an) (1 :: am)
+	| 1 :: tn, 0 :: tm => compare_borrow_m tn tm (1 :: dn) (0 :: dm) (0 :: an) (0 :: am)
+	| 0 :: tn, 1 :: tm => compare_borrow_n tn tm (0 :: dn) (1 :: dm) (0 :: an) (0 :: am)
+	end.
+
+Lemma compare_empty_decomp : forall x y n dn an rtn rdn ran,
+		(rtn, rdn, ran) = compare_empty n dn an ->
+		is_canonical_struct n -> n <> [] ->
+		length an = length dn ->
+		x = rev_append dn n ->
+		S (rev_nat an + to_nat y) = rev_nat (1 :: dn) ->
+		decomp x y rtn rdn ran.
+Proof.
+	intros x y n.
+	{	induction n as [|bn tn HR]; intros dn an rtn rdn ran He Hn Hz Hl Happ Hval;
+			[|destruct bn]; simpl in *.
+	+	contradiction.
+	+	assert (tn <> []) by (destruct tn; discriminate).
+		apply is_canonical_struct_tl in Hn.
+		assert (length (1 :: an) = length (0 :: dn)) by (simpl; rewrite Hl; reflexivity).
+		assert (S (rev_nat (1 :: an) + to_nat y) = rev_nat (1 :: 0 :: dn)).
+		{
+			simpl.
+			rewrite !to_nat_app, rev_length in Hval.
+			rewrite !to_nat_app, app_length, !rev_length.
+			rewrite Hl, (PeanoNat.Nat.add_comm _ 1).
+			simpl in *.
+			rewrite <- !plus_n_O in *.
+			rewrite PeanoNat.Nat.add_comm, PeanoNat.Nat.add_assoc, <- plus_Sn_m,
+			  (PeanoNat.Nat.add_comm (to_nat y)), Hval, PeanoNat.Nat.add_assoc.
+			reflexivity.
+		}
+		apply HR in He; assumption.
+	+	inversion_clear He.
+		split; assumption.
+	}
+Qed.
+
+
+
+Lemma compare_borrow_decomp_Lt : forall x y n m dn dm an am rtn rdn ran,
+		x = rev_append dn n -> y = rev_append dm m ->
+		is_canonical_struct n ->
+		is_canonical_struct m ->
+		length am = length dm ->
+		length am = length dn ->
+		(n <> [] ->
+		Some (Lt rtn rdn ran) = compare_borrow_n n m dn dm an am ->
+		S (rev_nat am + rev_nat dn) = rev_nat dm ->
+		decomp y x rtn rdn ran)
+		/\ (Some (Lt rtn rdn ran) = compare_borrow_m n m dn dm an am ->
+		S (rev_nat am + rev_nat dn) = rev_nat (1 :: dm) ->
+		decomp y x rtn rdn ran).
+Proof.
+	intros x y n.
+	{	induction n as [|bn tn HR]; intros m dn dm an am rtn rdn ran Hx Hy Hn Hm Hl1 Hl2;
+			[| destruct bn; apply is_canonical_struct_tl in Hn as Htn;
+		 		(destruct m as [|bm tm]; [|destruct bm;
+				apply is_canonical_struct_tl in Hm as Htm])];
+			pose proof (Hsl1 := (f_equal S) _ _ Hl1);
+			pose proof (Hsl2 := (f_equal S) _ _ Hl2);
+			(split; [intros He H Hval|intros H Hval]); simpl in *.
+	+	discriminate.
+	+  	destruct m; [discriminate|].
+		assert (b :: m <> []) by discriminate.
+		rewrite rev_append_rev, app_nil_r in Hx.
+		rewrite <- Hx in Hval.
+		assert (He : (rtn, rdn, ran) = compare_empty (b :: m) dm am)
+			by (destruct compare_empty as [r1 r3], r1; inversion H; reflexivity).
+		apply (compare_empty_decomp y x) in He; assumption.
+	+	destruct compare_empty as [r1 r3], r1.
+		discriminate.
+	+	discriminate.
+	+	apply HR in H; [assumption..| destruct tn; discriminate |].
+		simpl.
+		rewrite !to_nat_app, <- !plus_n_O.
+		assumption.
+	+ 	apply HR in H; [assumption..|].
+		simpl.
+		rewrite !to_nat_app, <- !plus_n_O in *.
+		rewrite PeanoNat.Nat.add_shuffle0, <- plus_Sn_m, Hval, last_length.
+		simpl.
+		rewrite <- !plus_n_O, PeanoNat.Nat.add_assoc, !rev_length, Hl1.
+		reflexivity.
+	+	apply HR in H; [assumption..| destruct tn; discriminate|].
+		simpl.
+		rewrite !to_nat_app, <- !plus_n_O in *.
+		rewrite PeanoNat.Nat.add_shuffle0, <- plus_Sn_m, Hval, !rev_length, Hl1.
+		reflexivity.
+	+	apply HR in H; [assumption..| destruct tn; discriminate|].
+		simpl.
+		rewrite !to_nat_app, <- !plus_n_O in *.
+		rewrite Hval.
+		reflexivity.
+	+	destruct tn; discriminate.
+	+	discriminate.
+	+	enough (S (rev_nat (1 :: am) + rev_nat (1 :: dn)) = rev_nat (1 :: 0 :: dm))
+			by (destruct tn; apply HR in H; assumption).
+		simpl.
+		rewrite !to_nat_app, last_length.
+		simpl.
+		rewrite <- !plus_n_O, PeanoNat.Nat.add_shuffle1, <- plus_Sn_m, Hval.
+		rewrite !rev_length, <- Hl2, Hl1.
+		reflexivity.
+	+	apply HR in H; [assumption..|].
+		simpl.
+		rewrite !to_nat_app, <- !plus_n_O in *.
+		rewrite PeanoNat.Nat.add_assoc, <- plus_Sn_m, Hval, last_length.
+		simpl.
+		rewrite <- !plus_n_O, PeanoNat.Nat.add_assoc, !rev_length.
+		rewrite <- Hl1, Hl2.
+		reflexivity.
+	+	{	destruct tn.
+		+	inversion_clear H.
+			enough (S (rev_nat am + to_nat x) = rev_nat (1 :: dm))
+				by (split; assumption).
+			simpl.
+			rewrite Hx, rev_append_rev, !to_nat_app, !rev_length.
+			simpl.
+			rewrite <- !plus_n_O, PeanoNat.Nat.add_assoc, <- plus_Sn_m, Hval, <- Hl2, Hl1.
+			reflexivity.
+		+	apply HR in H; [assumption..|discriminate|].
+			simpl.
+			rewrite !to_nat_app, !rev_length.
+			simpl.
+			rewrite <- !plus_n_O, PeanoNat.Nat.add_assoc, <- plus_Sn_m, Hval, <- Hl2, Hl1.
+			reflexivity.
+		}
+	+	destruct tm; [discriminate|].
+		apply HR in H; [assumption..| ].
+		simpl.
+		rewrite !to_nat_app, last_length, !rev_length in *.
+		simpl in *.
+		rewrite <- !plus_n_O in *.
+		rewrite <- PeanoNat.Nat.add_shuffle1, <- plus_Sn_m, Hval, <- Hl2, <- Hl1.
+		reflexivity.
+	}
+Qed.
+
+Lemma compare_decomp_Lt_aux : forall x y n m dn dm an am rtn rdn ran,
+		x = rev_append dn n -> y = rev_append dm m ->
+		is_canonical_struct n ->
+		is_canonical_struct m ->
+		length am = length dm ->
+		length am = length dn ->
+		Some (Lt rtn rdn ran) = compare n m dn dm an am ->
+		S (rev_nat am + rev_nat dn) = rev_nat (1 :: dm) ->
+		decomp y x rtn rdn ran.
+Proof.
+	intros x y n.
+	{	induction n as [|bn tn HR]; intros m dn dm an am rtn rdn ran Hx Hy Hn Hm Hl1 Hl2;
+			[| destruct bn; apply is_canonical_struct_tl in Hn as Htn];
+		 	(destruct m as [|bm tm]; [|destruct bm;
+				apply is_canonical_struct_tl in Hm as Htm]);
+			pose proof (Hsl1 := (f_equal S) _ _ Hl1);
+			pose proof (Hsl2 := (f_equal S) _ _ Hl2);
+			intros H Hval; simpl in *.
+	+	discriminate.
+	+	assert (tm <> []) by (destruct tm; discriminate).
+		assert (He : (rtn, rdn, ran) = compare_empty tm (0 :: dm) (1 :: am))
+		  by (destruct compare_empty as [r1 r3], r1; inversion H; reflexivity).
+		apply (compare_empty_decomp y x) in He; [assumption..|].
+		simpl.
+		rewrite !to_nat_app, last_length, !rev_length in *.
+		simpl in *.
+		rewrite <- !plus_n_O in *.
+		rewrite Hx, rev_append_rev, app_nil_r, PeanoNat.Nat.add_shuffle0, <- plus_Sn_m,
+			Hval, Hl1, PeanoNat.Nat.add_assoc.
+		reflexivity.
+	+	rewrite rev_append_rev, app_nil_r in Hx.
+		rewrite <- Hx in Hval.
+		inversion_clear H.
+		split; assumption.
+	+	destruct (compare_empty) as [r1 r3], r1; discriminate.
+	+	apply HR in H; [assumption..|].
+		simpl.
+		rewrite !to_nat_app, last_length, !rev_length in *.
+		simpl in *.
+		rewrite <- !plus_n_O in *.
+		rewrite PeanoNat.Nat.add_shuffle0, <- plus_Sn_m, Hval, Hl1,  PeanoNat.Nat.add_assoc.
+		reflexivity.
+	+	assert (tn <> []) by (destruct tn; discriminate).
+		apply (compare_borrow_decomp_Lt x y) in H; [assumption..|].
+		simpl.
+		rewrite !to_nat_app, !rev_length in *.
+		simpl in *.
+		rewrite <- !plus_n_O in *.
+		assumption.
+	+	discriminate.
+	+	apply (compare_borrow_decomp_Lt x y) in H; [assumption..|].
+		simpl.
+		rewrite !to_nat_app, last_length, !rev_length in *.
+		simpl in *.
+		rewrite <- !plus_n_O in *.
+		rewrite !PeanoNat.Nat.add_assoc, <- plus_Sn_m, Hval, <- Hl2, Hl1.
+		reflexivity.
+	+	apply HR in H; [assumption..|].
+		simpl.
+		rewrite !to_nat_app, last_length, !rev_length in *.
+		simpl in *.
+		rewrite <- !plus_n_O in *.
+		rewrite PeanoNat.Nat.add_shuffle1, <- plus_Sn_m, Hval, <- Hl2, Hl1.
+		reflexivity.
+	}
+Qed.
+
+
+Lemma compare_decomp_Lt : forall x y rtn rdn ran,
+		is_canonical x -> is_canonical y ->
+		Some (Lt rtn rdn ran) = compare x y [] [] [] [] ->
+		decomp y x rtn rdn ran.
+Proof.
+	intros x y rtn rdn ran Hx Hy H.
+	apply is_canonical_struct_equiv in Hx, Hy.
+	{	apply (compare_decomp_Lt_aux x y) in H.
+	+	assumption.
+	+	reflexivity.
+	+	reflexivity.
+	+	assumption.
+	+	assumption.
+	+	reflexivity.
+	+	reflexivity.
+	+	reflexivity.
+	}
+Qed.
+
+
+Lemma compare_borrow_sym : forall n m dn dm an am,
+		compare_borrow_n n m dn dm an am =
+			option_map comp_op (compare_borrow_m m n dm dn am an)
+		/\ compare_borrow_m n m dn dm an am
+			= option_map comp_op (compare_borrow_n m n dm dn am an).
+Proof.
+	intro n.
+	{	induction n as [|bn tn HR]; intros m dn dm an am; [|destruct bn];
+		(destruct m as [|bm tm]; [|destruct bm]); split; simpl; try reflexivity.
+	+	destruct compare_empty as [r1 r3], r1 as [r1 r2].
+		reflexivity.
+	+	destruct tm; reflexivity.
+	+	destruct compare_empty as [r1 r3], r1 as [r1 r2].
+		reflexivity.
+	+	apply HR.
+	+	apply HR.
+	+	apply HR.
+	+	destruct tm; apply HR.
+	+	destruct tn; reflexivity.
+	+	destruct tn; apply HR.
+	+	apply HR.
+	+	destruct tn; [reflexivity|apply HR].
+	+	destruct tm; [reflexivity|apply HR].
+	}
+Qed.
+	
+Lemma compare_sym : forall n m dn dm an am,
+		compare n m dn dm an am = option_map comp_op (compare m n dm dn am an).
+Proof.
+	intro n.
+	{	induction n as [|bn tn HR]; intros m dn dm an am; [|destruct bn];
+			(destruct m as [|bm tm]; [|destruct bm]); simpl.
+	+	reflexivity.
+	+	destruct compare_empty as [r1 r3], r1 as [r1 r2].
+		reflexivity.
+	+	reflexivity.
+	+	destruct compare_empty as [r1 r3], r1 as [r1 r2].
+		reflexivity.
+	+	apply HR.
+	+	apply compare_borrow_sym.
+	+	reflexivity.
+	+	apply compare_borrow_sym.
+	+	apply HR.
+	}
+Qed.
+
+Lemma compare_decomp_Gt : forall x y rtn rdn ran,
+		is_canonical x -> is_canonical y ->
+		Some (Gt rtn rdn ran) = compare x y [] [] [] [] ->
+		decomp x y rtn rdn ran.
+Proof.
+	intros x y rtn rdn ran Hx Hy H.
+	apply (f_equal (option_map comp_op)) in H.
+	rewrite <- compare_sym in H.
+	apply compare_decomp_Lt in H; assumption.
+Qed.
+
+Lemma compare_borrow_none : forall n m dn dm an am,
+		is_canonical_struct n -> is_canonical_struct m ->
+		(n <> [] -> compare_borrow_n n m dn dm an am <> None)
+		/\ (m <> [] -> compare_borrow_m n m dn dm an am <> None).
+Proof.
+	intros n.
+	{	induction n as [|bn tn HR]; intros m dn dm an am Hn Hm;
+			[| destruct bn; apply is_canonical_struct_tl in Hn as Htn];
+			(destruct m as [|bm tm]; [|destruct bm;
+				apply is_canonical_struct_tl in Hm as Htm]);
+	  		split; intro H; simpl; try contradiction.
+	+	discriminate.
+	+	discriminate.
+	+	discriminate.
+	+	assert (tn <> []) by (destruct tn; discriminate).
+		apply HR; assumption.
+	+	assert (tm <> []) by (destruct tm; discriminate).
+		apply HR; assumption.
+	+	assert (tn <> []) by (destruct tn; discriminate).
+		apply HR; assumption.
+	+	assert (tn <> []) by (destruct tn; discriminate).
+		apply HR; assumption.
+	+	destruct tn; discriminate.
+	+	assert (tm <> []) by (destruct tm; discriminate).
+		destruct tn; apply HR; assumption.
+	+	assert (tm <> []) by (destruct tm; discriminate).
+		apply HR; assumption.
+	+	destruct tn; [discriminate|].
+		apply HR; [assumption..|discriminate].
+	+	destruct tm; [discriminate|].
+		apply HR; [assumption..|discriminate].
+	}
+Qed.
+Lemma compare_none : forall n m dn dm an am,
+		is_canonical n -> is_canonical m ->
+		compare n m dn dm an am <> None.
+Proof.
+	intros n m dn dm an am Hn Hm.
+	apply is_canonical_struct_equiv in Hn, Hm.
+	revert m dn dm an am Hn Hm.
+	{	induction n as [|bn tn HR]; intros m dn dm an am Hn Hm;
+			[| destruct bn; apply is_canonical_struct_tl in Hn as Htn];
+			(destruct m as [|bm tm]; [|destruct bm;
+				apply is_canonical_struct_tl in Hm as Htm]);
+	  		simpl.
+	+	discriminate.
+	+	discriminate.
+	+	discriminate.
+	+	discriminate.
+	+	apply HR; assumption.
+	+	assert (tn <> []) by (destruct tn; discriminate).
+		apply compare_borrow_none; assumption.
+	+	discriminate.
+	+	assert (tm <> []) by (destruct tm; discriminate).
+		apply compare_borrow_none; assumption.
+	+	apply HR; assumption.
+	}
+Qed.
+Lemma compare_borrow_eq : forall n m dn dm an am,
+		compare_borrow_n n m dn dm an am <> Some Eq
+		/\ compare_borrow_m n m dn dm an am <> Some Eq.
+Proof.
+	intro n.
+	{	induction n as [|bn tn HR]; [|destruct bn];
+			(destruct m as [|bm tm]; [|destruct bm]); split; simpl;
+			try discriminate.
+	+	destruct compare_empty as [r1 r3], r1.
+		discriminate.
+	+	destruct compare_empty as [r1 r3], r1.
+		discriminate.
+	+	apply HR.
+	+	apply HR.
+	+	apply HR.
+	+	apply HR.
+	+	destruct tn; discriminate.
+	+	destruct tn; apply HR.
+	+	apply HR.
+	+	destruct tn; [discriminate| apply HR].
+	+	destruct tm; [discriminate| apply HR].
+	}
+Qed.
+Lemma compare_decomp_eq : forall n m dn dm an am,
+		compare n m dn dm an am = Some Eq <-> n = m.
+Proof.
+	intro n.
+	{	induction n as [|bn tn HR]; [|destruct bn];
+			(destruct m as [|bm tm]; [|destruct bm]);
+			split; intro H; simpl in *.
+	+	reflexivity.
+	+	reflexivity.
+	+	destruct compare_empty as [r1 r3], r1.
+		discriminate.
+	+	destruct compare_empty as [r1 r3], r1.
+		discriminate.
+	+	discriminate.
+	+	discriminate.
+	+	destruct compare_empty as [r1 r3], r1.
+		discriminate.
+	+	discriminate.
+	+	apply HR in H.
+		rewrite H.
+		reflexivity.
+	+	apply HR.
+		inversion_clear H.
+		reflexivity.
+	+	apply compare_borrow_eq in H.
+		contradiction.
+	+	discriminate.
+	+	discriminate.
+	+	discriminate.
+	+	apply compare_borrow_eq in H.
+		contradiction.
+	+	discriminate.
+	+	apply HR in H.
+		rewrite H.
+		reflexivity.
+	+	apply HR.
+		inversion_clear H.
+		reflexivity.
+	}
+Qed.
+
+
+Definition sub n m :=
+	match compare (trim n) (trim m) [] [] [] [] with
+	| Some (Gt rtn rdn ran) => trim (inc (rev_append ran (0 :: rtn))) 
+	| _ => []
+	end.
+
+Notation "n - m" := (sub n m) : bin_nat_scope.
+
+Theorem sub_minus : forall n m, to_nat (n - m) = (to_nat n - to_nat m)%nat.
+Proof.
+	intros n m.
+	unfold sub.
+	pose proof (Hn := trim_canonical n).
+	pose proof (Hm := trim_canonical m).
+	pose proof (Hnone := compare_none _ _ [] [] [] [] Hn Hm).
+	pose proof (Heq := compare_decomp_eq (trim n) (trim m) [] [] [] []).
+	pose proof (Hlt := compare_decomp_Lt (trim n) (trim m)).
+	pose proof (Hgt := compare_decomp_Gt (trim n) (trim m)).
+	rewrite <- (trim_nat n), <- (trim_nat m).
+	destruct compare as [comp|]; [|contradiction].
+	{	destruct comp as [|rtn rdn ran|rtn rdn ran].
+	+	rewrite (proj1 Heq); [|reflexivity].
+		rewrite PeanoNat.Nat.sub_diag.
+		reflexivity.
+	+	destruct (Hlt rtn rdn ran Hn Hm) as [Hl Hz Hv]; [reflexivity|].
+		apply (f_equal to_nat) in Hz.
+		rewrite rev_append_rev, to_nat_app in Hz.
+		simpl in Hz, Hv.
+		rewrite to_nat_app in Hv.
+		simpl in Hv.
+		rewrite <- !plus_n_O in *.
+		rewrite PeanoNat.Nat.add_assoc, <- Hv, <- plus_Sn_m,
+			<- PeanoNat.Nat.add_assoc, PeanoNat.Nat.add_comm, <- PeanoNat.Nat.add_assoc in Hz.
+		rewrite Hz, PeanoNat.Nat.sub_add_distr, PeanoNat.Nat.sub_diag, PeanoNat.Nat.sub_0_l.
+		reflexivity.
+	+	destruct (Hgt rtn rdn ran Hn Hm) as [Hl Hz Hv]; [reflexivity|].
+		rewrite Hz, trim_nat, inc_S, !rev_append_rev, !to_nat_app.
+		simpl in *.
+		rewrite !to_nat_app in Hv.
+		simpl in Hv.
+		rewrite <- !plus_n_O in *.
+		rewrite PeanoNat.Nat.add_assoc, <- Hv, <- !plus_Sn_m, PeanoNat.Nat.add_shuffle0,
+			PeanoNat.Nat.add_sub.
+		rewrite !rev_length, Hl.
+		reflexivity.
+	}
+Qed.
+
+
+(*Fixpoint sub_aux n m acc :=
 	match n, m with
 	| [], _ => None
 	| 0 :: tn, [] => sub_aux tn [] (1 :: acc)
@@ -429,6 +973,7 @@ Proof.
 		}
 	}
 Qed.
+ *)
 
 Module Notations.
 
--- theories/utils/Utils.v
+++ theories/utils/Utils.v
@@ -4,7 +4,7 @@ Import ListNotations.
 
 Section Option.
 
-Context {A : Type} (P : A -> Prop) (BP : A -> A -> Prop).
+Context {A : Type} (P : A -> Prop).
 
 Variant option_predicate : option A -> Prop :=
 	| OP_None : option_predicate None
@@ -16,17 +16,30 @@ Definition option_default d (o : option A) :=
 	| Some x => x
 	end.
 
-Variant option_bpredicate : option A -> option A -> Prop :=
+(*Variant option_bpredicate : option A -> option A -> Prop :=
 	| OBP_None : option_bpredicate None None
 	| OBP_Some : forall x y,
 		BP x y -> option_bpredicate (Some x) (Some y).
+ *)
+End Option.
 
-Lemma option_default_map_inv : forall d f (o : option A),
+Section Options.
+
+Context {A B C : Type} (P : B -> Prop) (f g : A -> B) (h : B -> C) (i : A -> A).
+
+Lemma option_map_map : forall o,
+		option_map h (option_map f o) = option_map (fun x => h (f x)) o.
+Proof.
+	intro o.
+	destruct o; reflexivity.
+Qed.
+
+Lemma option_default_map_inv : forall d (o : option A),
 	(o = None -> P d) ->
 	(forall x, o = Some x -> P (f x)) ->
 	P (option_default d (option_map f o)).
 Proof.
-	intros d f o Hd Hf.
+	intros d o Hd Hf.
 	{	destruct o; simpl.
 	+	apply Hf.
 		reflexivity.
@@ -34,19 +47,27 @@ Proof.
 		reflexivity.
 	}
 Qed.
-End Option.
 
-(*Section List.
-
-Context {A : Type} (P : A -> Prop).
-
-Fixpoint last_opt (l : list A) : option A :=
-	match l with
-	| [] => None
-	| [a] => Some a
-	| x :: tl => last_opt tl
-	end.
-
-Definition ends_pred l := option_predicate P (last_opt l).
-
-End List.*)
+Lemma option_map_inj : forall o1 o2,
+		(forall x y, (f x = f y -> x = y)) ->
+		option_map f o1 = option_map f o2 ->
+		o1 = o2.
+Proof.
+	intros o1 o2 Hinj H.
+	destruct o1, o2; [|discriminate..|reflexivity].
+	inversion H.
+	apply Hinj in H1.
+	rewrite H1.
+	reflexivity.
+Qed.
+Lemma option_map_inv : forall o,
+		(forall x, (i x = x)) ->
+		option_map i o = o.
+Proof.
+	intros o Hrew.
+	destruct o; [|reflexivity].
+	simpl.
+	rewrite Hrew.
+	reflexivity.
+Qed.
+End Options.
