e8bfbbe4d1428ad574018e2a3b7a80e12a6d4dd0
Titouan Quennet <titouanquennet@etu.u-paris.fr>
2024-06-14 20:20:32 +0200

    BinNat proofs complete

--- theories/numerical/Num.v
+++ theories/numerical/Num.v
@@ -1,4 +1,4 @@
-Require Import utils.Utils.
+Require Import utils.Utils Lists.List.
 
 Inductive Num (A : Type) :=
 | Ob : Num A
@@ -8,6 +8,14 @@ Definition t := Num.
 
 Arguments Ob {A}.
 Arguments snoc {A} n a.
+
+Fixpoint gplug {A} (snoc : Num A -> A -> Num A) (n: Num A)(ctxt: list A): Num A :=
+  match ctxt with
+  | nil => n
+  | cons b ctxt => gplug snoc (snoc n b) ctxt
+  end.
+Definition plug {A} : Num A -> list A -> Num A := gplug snoc.
+Notation rev := (plug Ob).
 
 Fixpoint length {A}(n : Num A): nat :=
   match n with
@@ -67,14 +75,122 @@ Definition foldir  {A B : Type} (f : nat -> B -> A -> B) (b: B)(n: Num A): B :=
 Definition Num_lift {A} (P : nat -> A -> Prop)(n: Num A): Prop :=
   foldMap Monoid_Prop P n.
 
+
 Fixpoint app {A} (m n : Num A): Num A :=
   match n with
   | Ob => m
   | snoc n a => snoc (app m n) a
   end.
 
-Fixpoint plug {A} (snoc : Num A -> A -> Num A) (n: Num A)(ctxt: list A): Num A :=
-  match ctxt with
-  | nil => n
-  | cons b ctxt => plug snoc (snoc n b) ctxt
-  end.
+Section to_list.
+
+Context {A : Type}.
+
+Definition to_list : Num A -> list A := foldr (fun l x => x :: l) nil.
+
+Lemma to_list_snoc : forall l b, to_list (snoc l b) = b :: to_list l.
+Proof.
+	intros l b.
+	unfold to_list.
+	rewrite fold_snoc.
+	reflexivity.
+Qed.
+
+Lemma to_list_plug : forall (l : Num A) dl, to_list (plug l dl) = rev_append dl (to_list l).
+Proof.
+	intros l dl.
+	revert l.
+	{	induction dl as [|bl tl HR]; intro l; simpl.
+	+	reflexivity.
+	+	cbn [plug gplug].
+		rewrite <- to_list_snoc.
+		apply HR.
+	}
+Qed.
+Lemma to_list_rev : forall (l : list A), to_list (rev l) = List.rev l.
+Proof.
+	intro l.
+	rewrite to_list_plug, rev_append_rev, app_nil_r.
+	reflexivity.
+Qed.
+Lemma to_list_app : forall (l r : Num A), to_list (app l r) = to_list r ++ to_list l.
+Proof.
+	intros l r.
+	revert l.
+	{	induction r as [|tr HR br]; intro l.
+	+	reflexivity.
+	+	cbn [app].
+		rewrite !to_list_snoc, <- app_comm_cons, HR.
+		reflexivity.
+	}
+Qed.
+
+
+Lemma to_list_inj : forall l r, to_list l = to_list r -> l = r.
+Proof.
+	intro l.
+	{	induction l as [|tl HR bl]; intros r H; destruct r as [|tr br].
+	+	reflexivity.
+	+	discriminate.
+	+	discriminate.
+	+	rewrite !to_list_snoc in H.
+		inversion H.
+		f_equal.
+		apply HR.
+		assumption.
+	}
+Qed.
+End to_list.
+
+(*
+Section app.
+
+Context {A : Type}.
+
+Lemma app_Ob_l : forall (n : Num A), app Ob n = n.
+Proof.
+	intros n.
+	{	induction n as [|tn HR bn]; simpl.
+	+	reflexivity.
+	+	rewrite HR.
+		reflexivity.
+	}
+Qed.
+
+
+Lemma plug_app : forall (n m : Num A) dn, plug (app n m) dn = app n (plug m dn).
+Proof.
+	intros n m dn.
+	revert n m.
+	{	induction dn as [|bn tn HR]; intros n m; simpl.
+	+	reflexivity.
+	+	cbn [plug Num.plug rev].
+		apply (HR n (snoc m bn)).
+	}
+Qed.
+
+Lemma plug_plug_app : forall (n : Num A) dn dm, plug (plug n dn) dm = plug n (dn ++ dm).
+Proof.
+	intros n dn dm.
+	revert n dn.
+	{	induction dm as [|bm tm HR]; simpl; intros n dn.
+	+	rewrite app_nil_r.
+		reflexivity.
+	+	cbn [plug gplug].
+		rewrite (HR .
+	}
+Qed.
+Lemma rev_app : forall (dn dm : list A), app (rev dn) (rev dm) = rev (dn ++ dm).
+Proof.
+	intros dn.
+	{	induction dn as [|bn tn HR]; intro dm; simpl.
+	+	rewrite app_Ob_l.
+		reflexivity.
+	+	cbn [rev gplug].
+		rewrite <- plug_app.
+		simpl.
+	}
+Qed.
+
+End app.*)
+
